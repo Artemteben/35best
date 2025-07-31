a82bda95e4203af6b50e54ca8ecf8a1f81895151
79096 <a.malakhinsky@gmail.com>
2025-07-31 20:25:31 +0300

    7

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 if "test" in sys.argv:
     DATABASES = {
         "default": {
-         "ENGINE": "django.db.backends.sqlite3",
+            "ENGINE": "django.db.backends.sqlite3",
         }
     }
 else:
